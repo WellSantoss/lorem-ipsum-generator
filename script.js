fc57d616c33723c05d1f83ce5816061a5ecc65da
Wellington Santos <wellington1998santoss@gmail.com>
2021-10-12 14:18:20 -0300

    finishing

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+import { LoremIpsum } from "lorem-ipsum";
 
+const lorem = new LoremIpsum();
+const btnGenerate = document.querySelector("button");
+const inputAmount = document.querySelector("#amount");
+const inputType = document.querySelector("#type");
+const divGenerated = document.querySelector(".generated");
+
+function showGenerated(element, content) {
+  const paragraph = document.createElement(element);
+  paragraph.innerText = content;
+  divGenerated.appendChild(paragraph);
+}
+
+function generate(amount, type) {
+  if (type === "words") {
+    showGenerated("span", lorem.generateWords(amount));
+  } else if (type === "sentences") {
+    showGenerated("span", lorem.generateSentences(amount));
+  } else if (type === "paragraphs") {
+    for (let i = 1; i <= amount; i += 1) {
+      showGenerated("p", lorem.generateParagraphs(1));
+    }
+  }
+}
+
+btnGenerate.addEventListener("click", (e) => {
+  e.preventDefault();
+  divGenerated.innerHTML = "";
+  generate(+inputAmount.value, inputType.value);
+});
